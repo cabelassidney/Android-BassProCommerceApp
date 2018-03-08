31bd13e20fd7913955c9852aef3534df461f2a5e
cabelassidney <cabelassidney@gmail.com>
2018-03-07 22:01:55 -0700

    second commit

--- app/src/main/java/com/basspro/bassprocommerce/localstorage/LoginModel.java
+++ app/src/main/java/com/basspro/bassprocommerce/localstorage/LoginModel.java
@@ -5,4 +5,29 @@ package com.basspro.bassprocommerce.localstorage;
  */
 
 public class LoginModel {
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    String username, password,email;
 }
--- app/src/main/java/com/basspro/bassprocommerce/localstorage/RegisterModel.java
+++ app/src/main/java/com/basspro/bassprocommerce/localstorage/RegisterModel.java
@@ -5,4 +5,60 @@ package com.basspro.bassprocommerce.localstorage;
  */
 
 public class RegisterModel {
+
+    String userName;
+    String password;
+    String dateOfBirth;
+    String latsName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getLatsName() {
+        return latsName;
+    }
+
+    public void setLatsName(String latsName) {
+        this.latsName = latsName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    String firstName;
+    String gender;
 }
