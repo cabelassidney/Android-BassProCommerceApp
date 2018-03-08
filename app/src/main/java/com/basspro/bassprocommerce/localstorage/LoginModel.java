735c24d9f34544cc93a32c75e46dfb4afae8cbd1
venkata36 <mvenkata36@gmail.com>
2018-03-07 21:45:07 -0700

    first commit

---
+++ app/src/main/java/com/basspro/bassprocommerce/localstorage/LoginModel.java
@@ -0,0 +1,8 @@
+package com.basspro.bassprocommerce.localstorage;
+
+/**
+ * Created by manep on 3/7/2018.
+ */
+
+public class LoginModel {
+}
---
+++ app/src/main/java/com/basspro/bassprocommerce/util/AppPreferences.java
@@ -0,0 +1,8 @@
+package com.basspro.bassprocommerce.util;
+
+/**
+ * Created by manep on 3/7/2018.
+ */
+
+public class AppPreferences {
+}
